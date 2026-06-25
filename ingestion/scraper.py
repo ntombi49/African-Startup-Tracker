5dd25e84569bfcab3e2f3bb09a80475fd9f0d5cc
Twanano Rikhotso <ntwananorikhotso05@gmail.com>
2026-06-25 11:25:35 +0200

    funding cleaning function

--- ingestion/scraper.py
+++ ingestion/scraper.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = "https://example.com"
@@ -8,3 +9,13 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 print(soup.title.text)
+
+def clean_funding(text):
+    numbers = re.sub(r'[^0-9]', '', text)
+
+    if numbers:
+        return int(numbers)
+
+    return 0
+
+print(clean_funding("$2,500,000"))
