91d27eafa27a255a02e367d4e1cf888700947e6b
Twanano Rikhotso <ntwananorikhotso05@gmail.com>
2026-06-27 18:28:00 +0200

    created Spring Boot project

---
+++ backend/src/test/java/com/africa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.africa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
